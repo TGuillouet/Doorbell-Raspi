640373227906c8fd9b1005d84f521acaac7d8e92
Thomas Guillouët <thomas.guillouet@outlook.com>
2018-10-03 13:33:49 +0200

    Add listener

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const Sound = require('aplay');
+const app = require("express")();
+const http = require("http").Server(app);
+const Sound = require("aplay");
 
 var isPlaying = false;
 
-/* http.listen(3000, function(){
+http.listen(3000, function(){
+    console.log("Begin");
     if (isPlaying === false) {
         playSound('NeedBackup.wav');
     }
-}); */
-
-if (isPlaying === false) {
-    playSound('NeedBackup.wav');
-}
+});
 
 function playSound(soundName) {
-    isPlaying = true;
-    // with ability to pause/resume:
-    var music = new Sound();
-    music.play(__dirname + '/Sounds/' + soundName);
-    console.log(music);
-    // you can also listen for various callbacks:
-    music.on('complete', function () {
-        setTimeout(() => {
-            isPlaying = false;
-        }, 10000);
-    });
+  isPlaying = true;
+  // with ability to pause/resume:
+  var music = new Sound();
+  music.play(__dirname + "/Sounds/" + soundName);
+  console.log(music);
+  // you can also listen for various callbacks:
+  music.on("complete", function() {
+    setTimeout(() => {
+      isPlaying = false;
+    }, 10000);
+  });
 }
